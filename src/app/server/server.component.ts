c7ba10358bacf85a6d158ebb99b99cf254e61424
sanjay chakraborty <chakraborty.sanjayy@gmail.com>
2020-03-01 22:32:01 +0530

    [L 38] 38.styling element Dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-server',
-  templateUrl:'server.component.html'  
+  selector: 'app-server',
+  templateUrl: 'server.component.html'
 })
 export class ServerComponent {
-  serverID:Number = 10;
-  serverName:String = 'Test';
-  serverStatus:String = "Offline";
+  serverID = 10;
+  serverName = 'Test';
+  serverStatus = 'Offline';
+
+  constructor() {
+    //  Math.random() function returns a floating-point, pseudo-random number in the range 0 to less than 1
+   this.serverStatus = Math.random() >  0.5 ? 'Online' : 'Offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
+  }
 }
